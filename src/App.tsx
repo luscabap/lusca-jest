728466e161556259abfe2a32f77376b7a12f160e
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-02-29 18:44:41 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Formulario from './components/Cabecalho/indes';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Formulario.tsx
@@ -0,0 +1,11 @@
+const Formulario = () =>{
+    return (
+        <form action="">
+            <input type="text" name="" id="" placeholder="Insira os nomes dos participantes"/>
+            <button disabled={true}>Adicionar</button>
+        </form>
+    );
+    
+}
+
+export default Formulario;
